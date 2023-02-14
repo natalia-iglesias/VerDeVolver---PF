ce6fdb84aca50ea27689ec72730602895fe08ea0
toleZ <toloyjc@gmail.com>
2023-02-14 16:42:35 -0300

    armar ruteo de la app - Front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,36 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import Map from './pages/Map';
+import Service from './pages/Service';
+import Entities from './pages/Entities';
+import EntitieDetail from './pages/EntitieDetail';
+import SingUpEntitie from './pages/SingUpEntitie';
+import LogIn from './pages/LogIn';
+import Profile from './pages/Profile';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/entities" element={<Entities />} />
+        <Route path="/entitie/:id" element={<EntitieDetail />} />
+        <Route path="/beVdV" element={<SingUpEntitie />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
