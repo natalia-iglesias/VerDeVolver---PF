7144e19af319b0d309eb2a77d10b414afb8f8b53
DamGarA <damiangarciaabreu@gmail.com>
2023-02-14 22:36:29 -0300

    entitie detail page

--- client/src/pages/Entities.jsx
+++ client/src/pages/Entities.jsx
@@ -1,38 +1,5 @@
-import React from 'react';
-import { Box, Flex, Image } from '@chakra-ui/react';
+const Entities = () => {
+  return <h1>Entities</h1>;
+};
 
-function EntitieDetail() {
-  return (
-    <>
-      <Flex
-        id="entitieDetail"
-        justifyContent="space-around"
-        flexDirection="row"
-        height="100vh"
-      >
-        <Flex
-          id="leftColumn"
-          justifyContent="space-around"
-          flexDirection="column"
-        >
-          <Image
-            boxSize="300px"
-            objectFit="cover"
-            src="https://www.semm.com.uy/wp-content/uploads/2021/11/img-nota-ecologia.png"
-            alt="Entitie image"
-          />
-        </Flex>
-        <Flex
-          id="righttColumn"
-          justifyContent="space-around"
-          flexDirection="column"
-        >
-          <Box>nombre</Box>
-          <Box>descripcion</Box>
-        </Flex>
-      </Flex>
-    </>
-  );
-}
-
-export default EntitieDetail;
+export default Entities;
