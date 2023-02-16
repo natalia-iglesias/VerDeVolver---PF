cb17f1c8834fbb137bf2f1b859a90953ceceda32
SolanaG <gomez.solanarocio@gmail.com>
2023-02-15 23:16:57 -0300

    form singup entity funcionalidad

--- client/src/pages/SingUpEntitie.jsx
+++ client/src/pages/SingUpEntitie.jsx
@@ -1,4 +1,5 @@
 import { materials } from '../db.json';
+import { useState } from 'react';
 
 import {
   FormControl,
@@ -16,41 +17,58 @@ import {
 } from '@chakra-ui/react';
 
 const SingUpEntitie = () => {
-  // const [form, setForm] = useState({
-  //   name: '',
-  //   email: '',
-  //   adress: '',
-  //   image: '',
-  //   cbu: '',
-  //   alias: '',
-  //   material: [],
-  //   description: '',
-  // });
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    adress: '',
+    imageCloud: '',
+    cbu: '',
+    alias: '',
+    materials: [],
+    description: '',
+  });
+  // const [materials, setMaterials] =useState([])
+
+  const handlerChange = (e) => {
+    console.log('e::', e);
+    let keyValue;
+    if (typeof e === 'string') {
+      keyValue = { ['materials']: [...form.materials, e] };
+    } else {
+      const { name, value } = e.target;
+      keyValue = { [name]: value };
+    }
+
+    setForm({ ...form, ...keyValue });
+  };
+  console.log(form);
+
+  const handlerSubmit = (event) => {
+    event.preventDefault();
+    console.log('submit');
+  };
 
   return (
-    <FormControl isRequired>
+    <FormControl margin="3%" isRequired onSubmit={handlerSubmit}>
       <FormLabel>Nombre</FormLabel>
-      <Input type="name" />
+      <Input name="name" onChange={handlerChange} type="text" />
       <FormLabel>Email</FormLabel>
-      <Input type="email" />
+      <Input name="email" onChange={handlerChange} type="email" />
       <FormLabel>Dirección</FormLabel>
-      <Input type="adress" />
+      <Input name="adress" onChange={handlerChange} type="text" />
       <FormLabel>Imagen</FormLabel>
-      <Input type="image" />
+      <Input name="image" onChange={handlerChange} type="text" />
       <FormLabel requiredIndicator>CBU</FormLabel>
-      <Input type="cbu" />
-      <FormHelperText>
-        Completa este campo si estás de acuerdo en recibir donaciones.
-      </FormHelperText>
+      <Input name="cbu" onChange={handlerChange} type="number" />
       <FormLabel requiredIndicator>Alias</FormLabel>
-      <Input type="alias" />
+      <Input name="alias" onChange={handlerChange} type="text" />
       <FormLabel>Materiales Reciclables</FormLabel>
-      <RadioGroup defaultValue="Paper">
+      <RadioGroup name="materials" onChange={handlerChange}>
         <HStack>
           <Grid templateColumns="repeat(4, 7fr)" gap={6}>
             {materials?.map((m, i) => (
               <GridItem key={i} w="100%" h="10" width={'auto'}>
-                <Radio key={i} colorScheme="green">
+                <Radio key={i} colorScheme="green" value={m}>
                   {m}
                 </Radio>
               </GridItem>
@@ -61,8 +79,14 @@ const SingUpEntitie = () => {
       <FormHelperText>
         Selecciona únicamente los materiales que recibirás.
       </FormHelperText>
+      <br />
       <FormLabel>Descripción</FormLabel>
-      <Textarea placeholder="Ingresa una descripción..." />
+
+      <Textarea
+        onChange={handlerChange}
+        name="description"
+        placeholder="Ingresa una descripción..."
+      />
       <Button colorScheme="green" type="submit">
         Enviar
       </Button>
