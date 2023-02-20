c04fcaa5c231f916b9cb1873d9e124b7ad161cb9
SolanaG <gomez.solanarocio@gmail.com>
2023-02-20 11:39:10 -0300

    modify-nav-home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import {
   Box,
   InputLeftElement,
   HStack,
+  Heading,
 } from '@chakra-ui/react';
 import PostsCarousel from '../components/PostsCarousel';
 import { MdOutlineAttachMoney } from 'react-icons/md';
@@ -17,9 +18,27 @@ const Home = () => {
 
   return (
     <Box>
+      <Heading
+        as="h1"
+        size="l"
+        display="flex"
+        align="center"
+        justify="center"
+        bg="#68D391"
+        w="auto"
+        h="100px"
+        color="white"
+        padding="2%"
+      >
+        Te brindamos información sobre los distintos lugares dedicados al
+        reciclaje en todo el país. Encontrá los más cercanos y hacé que tu
+        experiencia de gestión de residuos sea mucho más fácil. ¡Gracias por
+        cuidar el planeta junto a nosotrxs!
+      </Heading>
+
       <Stack p={'4'}>
         <HStack>
-          <Select placeholder="Entidad a donar">
+          <Select placeholder="Colabora con el punto de reciclaje que te haya ayudado ">
             {entities?.map(({ uuid, name }) => (
               <option key={uuid}>{name}</option>
             ))}
