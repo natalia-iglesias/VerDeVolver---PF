d391c1076774dd30d8cf4db1107011f3ed6d4b38
DamGarA <damiangarciaabreu@gmail.com>
2023-02-24 05:31:30 -0300

    mapeo de los marcadores de entidad

--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -1,27 +1,78 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
-import AsideMap from '../Components/AsideMap';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import MainMapComponent from '../Components/MainMapComponent';
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api';
+import MarkerInfo from '../Components/MarkerInfo';
 
 const containerStyle = {
-  width: '400px',
-  height: '400px',
+  width: '99vw',
+  height: '100vh',
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523,
+  lat: -39,
+  lng: -64,
 };
 
+const markersData = [
+  {
+    id: 1,
+    position: { lat: -34, lng: -64 },
+    name: 'Economía Circular',
+    description:
+      'Entidad privada cuya actividad se centra en mejorar la calidad de vida de las personas optimizando el aprovechamiento de recursos.',
+    imageUrl:
+      'https://i.pinimg.com/564x/0e/60/c7/0e60c7fcd2d898873fc7d1a5060cc232.jpg',
+    materials: ['Plástico', 'Vidrio', 'Cartón'],
+  },
+  {
+    id: 2,
+    position: { lat: -35, lng: -65 },
+    name: 'Amigos de la Tierra',
+    description:
+      'Somos una asociación ecologista que fomenta el cambio local y global hacia una sociedad respetuosa con el medio ambiente, justa y solidaria.',
+    imageUrl:
+      'https://i.pinimg.com/564x/4e/db/10/4edb108418125c6085492f82349de7b2.jpg',
+    materials: ['Vidrio', 'Papel', 'Pilas'],
+  },
+];
+
 const Map = () => {
+  const [activeMarker, setActiveMarker] = React.useState(null);
+
+  const handleMarkerMouseOver = (marker) => {
+    setActiveMarker({ ...marker });
+  };
+
+  const handleMarkerMouseOut = () => {
+    setTimeout(() => setActiveMarker(null), 1500);
+  };
+
   return (
-    <LoadScript googleMapsApiKey="AIzaSyDrsME92YLQUVHQ7gIROn5_Sx9xQZEcwS0">
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-        {/* Child components, such as markers, info windows, etc. */}
-        <></>
-      </GoogleMap>
-    </LoadScript>
+    <Box>
+      <LoadScript googleMapsApiKey="AIzaSyDrsME92YLQUVHQ7gIROn5_Sx9xQZEcwS0">
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={5}>
+          {markersData.map((marker) => (
+            <Marker
+              key={marker.id}
+              position={marker.position}
+              onMouseOver={() => handleMarkerMouseOver(marker)}
+              onMouseOut={handleMarkerMouseOut}
+            />
+          ))}
+
+          {activeMarker && (
+            <InfoWindow position={activeMarker.position}>
+              <MarkerInfo data={activeMarker} />
+            </InfoWindow>
+          )}
+        </GoogleMap>
+      </LoadScript>
+    </Box>
   );
 };
 
