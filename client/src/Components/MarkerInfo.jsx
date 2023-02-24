292878cc31743c52755d7b2f57c3c3254325aabc
DamGarA <damiangarciaabreu@gmail.com>
2023-02-24 20:42:33 -0300

    cambio en los modelos para renderizar los marcadores y autocomplete

--- client/src/Components/MarkerInfo.jsx
+++ client/src/Components/MarkerInfo.jsx
@@ -6,10 +6,10 @@ function MarkerInfo({ data }) {
     <div>
       <h2>{data.name}</h2>
       <p>{data.description}</p>
-      <img src={data.imageUrl} alt={data.title} width="50px" height="50px" />
+      <img src={data.img} alt={data.name} width="50px" height="50px" />
       <Flex>
-        {data.materials.map((mat, index) => (
-          <p key={index}>{`${mat}--`}</p>
+        {data.Materials.map((mat, index) => (
+          <p key={index}>{`${mat.name}--`}</p>
         ))}
       </Flex>
     </div>
