59cec4f1d989cafc7afc425b3388254984d8a068
toleZ <toloyjc@gmail.com>
2023-03-04 12:14:30 -0300

    footer position fix - front

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -5,13 +5,14 @@ const Footer = () => {
   return (
     <Box>
       <Flex
-        alignItems={'center'}
+        alignItems="center"
         justifyContent="center"
-        width={'100%'}
-        h="4rem"
+        width="full"
+        p="1rem"
         bg="#80808080"
-        color={'white'}
-        bottom={'0'}
+        color="#e4ebed"
+        pos="absolute"
+        bottom="0"
       >
         <Text fontSize="l">
           © 2023 VerDeVolver. All rights reserved. Designed and developed by
