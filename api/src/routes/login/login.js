6b21747a7b9a46e03d5b2ef6b645501bd7995f55
toleZ <toloyjc@gmail.com>
2023-03-08 16:21:27 -0300

    googleLogin route redirect - back

--- api/src/routes/login/login.js
+++ api/src/routes/login/login.js
@@ -61,7 +61,7 @@ router.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
-    res.redirect(`https://ver-de-volver-pf-psi.vercel.app/login`);
+    res.redirect(`https://ver-de-volver-pf-98gc.vercel.app/login`);
   }
 );
 
